e52e823ff313e52ec1425358ef1506a05a8405ef
x649615715 <649615715@qq.com>
2019-12-06 13:21:08 +0800

    new

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,15 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  strict: true,
   state: {
     name: 'astea',
-    data: 1
+    data: 1,
+    everydayObj: {
+      contenten: "Good teachers, they inspire you, they entertain you, and you end up learning a ton even when you don't know it.",
+      contentzh: "好的老师会激励你，给你带来欢乐，悄然无声地教会你很多东西。",
+      author: "———— Nicholas Sparks"
+    }
   },
   mutations: {
     setData (state, data) {
